dca53d289681fe95281b9b33698e6ba778ceff9a
Cyril Beslay <cicoub13@gmail.com>
2020-11-11 18:46:23 +0100

    change sounds

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-var audioSymetricOne = new Audio('./audio/audio_file_1.mp3');
-var audioSymetricTwo = new Audio('./audio/audio_file_2.mp3');
+const audioSymetricOne = new Audio('./audio/inspire.mp3');
+const audioSymetricTwo = new Audio('./audio/expire.mp3');
 
-const audioTriangleOne = new Audio();
-const audioTriangleTwo = new Audio();
-const audioTriangleThree = new Audio();
+const audioTriangleOne = new Audio('./audio/hold.mp3');
+const audioTriangleTwo = new Audio('./audio/expire.mp3');
+const audioTriangleThree = new Audio('./audio/inspire.mp3');
 
-const audioSquareOne = new Audio();
-const audioSquareTwo = new Audio();
-const audioSquareThree = new Audio();
-const audioSquareFour = new Audio();
+const audioSquareOne = new Audio('./audio/hold.mp3');
+const audioSquareTwo = new Audio('./audio/expire.mp3');
+const audioSquareThree = new Audio('./audio/hold.mp3');
+const audioSquareFour = new Audio('./audio/inspire.mp3');
 
 let instruction = "";
 
@@ -26,36 +26,55 @@ function countdownTimer(remainingTime, breathingType) {
                 if (remainingTime % 10 === 0){
                     audioSymetricOne.play();
                     instruction = "Inspirez";
+                    document.getElementById("visual").classList.add('bigger');
+                    document.getElementById("visual").classList.remove('smaller');
                 } else if (remainingTime % 5 === 0){
                     audioSymetricTwo.play();
                     instruction = "Expirez";
+                    document.getElementById("visual").classList.add('smaller');
+                    document.getElementById("visual").classList.remove('bigger');
                 }
                 break;
             case 'TRIANGLE':
                 if (remainingTime % 15 === 0){
                     audioTriangleThree.play();
                     instruction = "Inspirez";
+                    document.getElementById("visual").classList.add('bigger');
+                    document.getElementById("visual").classList.remove('smaller');
+                    document.getElementById("visual").classList.remove('hold');
                 } else if (remainingTime % 10 === 0){
                     audioTriangleTwo.play();
                     instruction = "Expirez";
+                    document.getElementById("visual").classList.add('smaller');
+                    document.getElementById("visual").classList.remove('bigger');
+                    document.getElementById("visual").classList.remove('hold');
                 } else if (remainingTime % 5 === 0){
                     audioTriangleOne.play();
                     instruction = "Bloquez";
+                    document.getElementById("visual").classList.add('hold');
                 }
                 break;
             case 'SQUARE':
                 if (remainingTime % 20 === 0){
                     audioSquareFour.play();
                     instruction = "Inspirez";
+                    document.getElementById("visual").classList.add('bigger');
+                    document.getElementById("visual").classList.remove('smaller');
+                    document.getElementById("visual").classList.remove('hold');
                 } else if (remainingTime % 15 === 0){
                     audioSquareThree.play();
                     instruction = "Bloquez";
+                    document.getElementById("visual").classList.add('hold');
                 } else if (remainingTime % 10 === 0){
                     audioSquareTwo.play();
                     instruction = "Expirez";
+                    document.getElementById("visual").classList.add('smaller');
+                    document.getElementById("visual").classList.remove('bigger');
+                    document.getElementById("visual").classList.remove('hold');
                 } else if (remainingTime % 5 === 0){
                     audioSquareOne.play();
                     instruction = "Bloquez";
+                    document.getElementById("visual").classList.add('hold');
                 }
                 break;
         }
@@ -79,6 +98,7 @@ function countDown() {
     document.getElementById("pause").style.visibility = "visible";
     document.getElementById("start").style.visibility = "hidden";
     document.getElementById("configuration").style.visibility = "hidden";
+
     let remainingTime = minutes * 60;
     document.getElementById("progress").max = remainingTime;
     remainingTime = countdownTimer(remainingTime, breathingType);
